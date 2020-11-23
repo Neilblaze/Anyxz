114adb58a86499c0b5955199d56818d371391e0e
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-11-23 18:15:57 +0530

    p5js init
    • Setup
    • Draw
    • Get Result
    • Run once
    • Segment img

--- index.js
+++ index.js
@@ -69,3 +69,58 @@ const captureInterval = setInterval(() => {
 
 // Add auto reload function upon t = 30
 
+//p5js initial setup
+function setup() {
+  createCanvas(540, 400);
+  video = createCapture(VIDEO); // check out the stream, check if you can find the difference between no stream vs stream log
+  video.size(200, 150); // displaying the main image(video) on the side
+  video.class("webcam-feed");
+  sgImage = createImage(width, height);
+  uNet.segment(video, gotResult); // initial segmentation
+  bg = loadImage("./assets/loadingx.jpg"); // initial loading image
+}
+
+//adding the dom elements, from p5js. this function runs continuously
+function draw() {
+  background(bg);
+  image(sgImage, 0, 0, width, height);
+}
+
+function gotResult(error, result) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  //showing webcam permission warning error
+  if (!chkCameraPermissionOnce && runWebcamWarningOnce) {
+    webcamWarningModal.show();
+    runWebcamWarningOnce = false;
+  }
+
+  // Refactor required!
+
+  //checking if the user gave permission
+  if (video) {
+    video.loadPixels();
+    //checking if user gave permission
+    if (!(video.pixels[1] > 0) && chkCameraPermissionOnce) {
+      console.error("User didn't gave permisson");
+      chkCameraPermissionOnce = false;
+    }
+  }
+
+  //doing stuff after the initial uNet model has loaded and working, running this only once
+  if (!runitOnceUnet) {
+    runitOnceUnet = true;
+    bg = "#34eb89"; //initial image (parrot-greem)
+    const video = document.querySelector("video"); //getting the video after its created by p5js
+    video.parentNode.insertBefore(eventContainer, video.nextSibling); //inserting the eventContainer after the video element [https://stackoverflow.com/questions/4793604/how-to-insert-an-element-after-another-element-in-javascript-without-using-a-lib]
+    eventContainer.style.display = "block";
+    loadingTime.style.display = "none";
+    clearInterval(captureInterval);
+  }
+  sgImage = result.backgroundMask;
+  uNet.segment(video, gotResult);
+}
+
+
