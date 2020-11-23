38fb4b6dab64f8252ee7e7018e693c6e5994642d
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-11-23 18:12:16 +0530

    set objects

--- index.js
+++ index.js
@@ -12,4 +12,25 @@ const backgroundColour = document.querySelector("#background-color-picker");
 const recIconClone = document.querySelector("#record-icon").cloneNode(true);
 const loadingTime = document.querySelector("#loading-time");
 
+let video;
+let uNet;
+let sgImage;
+let bg;
+let runitOnceUnet = false;
+let recording = false; // initial state = 0
+let recorder;
+const chunks = [];
+let updateTimer;
+let timeCount = 30; //loading time component
+let chkCameraPermissionOnce = true;
+let runWebcamWarningOnce = true;
 
+console.log("=> 2940");
+
+// BOOTSTRAP UTILS
+const VidInsPopover = new bootstrap.Popover(
+  document.querySelector(".popover-dismiss-vid"),
+  {
+    trigger: "focus",
+  }
+);
